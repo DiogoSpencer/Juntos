4910026f99af94090f67e8aac23d6b03e6f564c3
Niennaz <nicole_oix@hotmail.com>
2021-06-09 02:05:29 +0200

    updatedFAQ

--- src/components/FAQ/FAQ.js
+++ src/components/FAQ/FAQ.js
@@ -1,22 +1,37 @@
 import { Fragment } from "react";
 import { Link } from "react-router-dom";
 import Button from "../UI/Button";
-import Collapsible from "./Collapsible";
+import FaqCard from "./FaqCard";
 
-const text = "Não encontras o que procuras?"
+const text = "Não encontras o que procuras?";
+
+//esta data vai ver do servidor?
+const data = [
+  { text: "Como instalar?", info: "Para instalar a app basta descarregar" },
+  { text: "Como instalar?", info: "Para instalar a app basta descarregar" },
+  { text: "Como instalar?", info: "Para instalar a app basta descarregar" },
+  { text: "Como instalar?", info: "Para instalar a app basta descarregar" },
+  { text: "Como instalar?", info: "Para instalar a app basta descarregar" },
+];
 
 const FAQ = () => {
   return (
     <Fragment>
       <h1>FAQ</h1>
-      <Collapsible text="Como instalar?">
-        <div>
-            <p>Para instalar a app basta descarregar</p>
-        </div>
-      </Collapsible>
-      <Link to="/Contactos">
-        <Button text={text}/>
-      </Link>
+      <div>
+        {data.map((faqQuestion, index) => {
+          return (
+            <div key={index}>
+              <FaqCard text={faqQuestion.text} info={faqQuestion.info} />
+            </div>
+          );
+        })}
+      </div>
+      <div>
+        <Link to="/Contactos">
+          <Button text={text} />
+        </Link>
+      </div>
     </Fragment>
   );
 };
