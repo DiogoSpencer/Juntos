e188faf5d7ab3979034cd0072c9e121d5745c7e6
Niennaz <nicole_oix@hotmail.com>
2021-06-24 01:32:44 +0200

    intro style

--- src/components/Home/Intro.js
+++ src/components/Home/Intro.js
@@ -1,27 +1,31 @@
+import classes from "./Intro.module.css";
+
 const Intro = () => {
   return (
-    <div>
-      <div>
-        <img src="" alt="holdingHands" />
-        <h1>Temos um longo caminho pela frente</h1>
-        <h1>
-          ... e finalmente podemos caminhar <span>juntos</span>
+    <div className={classes.containerDiv}>
+      <div className={classes.mainImageDiv}></div>
+      <div className={classes.hDiv}>
+        <h1 className={classes.hFirst}>Temos um longo caminho pela frente</h1>
+        <h1 className={classes.hSecond}>
+          ... e finalmente podemos caminhar{" "}
+          <span className={classes.juntosTitle}>juntos</span>
         </h1>
       </div>
-      <div>
+      <div className={classes.mainContent}>
         <p>
-          <span>juntos</span> é um projeto que procura ajudar após a pandemia
-          SARS-CoV-2.
+          <span className={classes.juntos}>juntos</span> é um projeto que
+          procura ajudar após a pandemia SARS-CoV-2.
         </p>
         <p>
-          <span>juntos</span> procura incentivar a cooperação e entreajuda de
-          todos para todos.
+          <span className={classes.juntos}>juntos</span> procura incentivar a
+          cooperação e entreajuda de todos para todos.
         </p>
         <p>
-          Finalmente podemos caminhar <span>juntos</span> para um futuro melhor.
+          Finalmente podemos caminhar{" "}
+          <span className={classes.juntos}>juntos</span> para um futuro melhor.
         </p>
       </div>
-      <div>
+      <div className={classes.mainVideo}>
         <video width="320" height="240" controls>
           <source src="" type="video/mp4" />O teu browser não suporta video
         </video>
