764b5c647ca0204e19fc0d4db289031c6d4ea0dd
Niennaz <nicole_oix@hotmail.com>
2021-06-24 14:12:44 +0200

    agradecimentos style

--- src/components/Home/Agradecimentos.js
+++ src/components/Home/Agradecimentos.js
@@ -1,42 +1,47 @@
 import { Link } from "react-router-dom";
-import Icon from "../UI/Icon";
+import classes from "./Agradecimentos.module.css";
+import heart from "../../img/heart.png"
+import nova from "../../img/nova_4.png"
+
 
 const Agradecimentos = () => {
   //todos estes divs vao ser links para paginas dos parceiros
   return (
-    <div id="parceiros">
-      <img src="" alt="fist-bump" />
-      <div>
-        <p>
-          Estes são os nossos parceiros, sem eles não estaríamos{" "}
-          <span>juntos</span>
-        </p>
-        <div>
-          <div>
-            <Icon xmlns="" viewBox="" fill="" d="" />
-          </div>
-          <div>
-            <Icon xmlns="" viewBox="" fill="" d="" />
-          </div>
-          <div>
-            <Icon xmlns="" viewBox="" fill="" d="" />
-          </div>
-          <div>
-            <Icon xmlns="" viewBox="" fill="" d="" />
-          </div>
-          <div>
-            <Icon xmlns="" viewBox="" fill="" d="" />
-          </div>
-          <div>
-            <Icon xmlns="" viewBox="" fill="" d="" />
-          </div>
+    <div id="parceiros" className={classes.mainParceiros}>
+      <h1 className={classes.mainTitle}>Agradecimentos</h1>
+      <p className={classes.descriptionTextParceiros}>
+        Estes são os nossos parceiros, sem eles não estaríamos{" "}
+        <span className={classes.juntos}>juntos</span>
+      </p>
+      <div className={classes.allLogos1}>
+        <div className={classes.logo}>
+          <img src={nova} alt="logo-1" className={classes.logoImg} />
         </div>
-        <div>
-          <h3>Obrigado</h3>
-          <Icon xmlns="" viewBox="" fill="" d="" />
+        <div className={classes.logo}>
+          <img src={nova} alt="logo-1" className={classes.logoImg} />
         </div>
-        <Link to="/Juntar">JUNTA-TE A NÓS</Link>
+        <div className={classes.logo}>
+          <img src={nova} alt="logo-1" className={classes.logoImg} />
+        </div>
       </div>
+      <div className={classes.allLogos2}>
+        <div className={classes.logo}>
+          <img src={nova} alt="logo-2" className={classes.logoImg} />
+        </div>
+        <div className={classes.logo}>
+          <img src={nova} alt="logo-2" className={classes.logoImg} />
+        </div>
+        <div className={classes.logo}>
+          <img src={nova} alt="logo-2" className={classes.logoImg} />
+        </div>
+      </div>
+      <div className={classes.thanks}>
+        <h3 className={classes.thanksText}>Obrigado</h3>
+        <img src={heart} alt="heart" className={classes.heartImg} />
+      </div>
+      <Link to="/Juntar" className={classes.joinLink}>
+        Junta-te a Nós
+      </Link>
     </div>
   );
 };
--- src/components/Home/HeroisHome.js
+++ src/components/Home/HeroisHome.js
@@ -1,24 +1,30 @@
 import { Link } from "react-router-dom"
+import oceanSaving from "../../img/oceanSaving.jpg";
+import boxesVolunteer from "../../img/boxesVolunteer.jpg";
+import foodVolunteer from "../../img/foodVolunteer.jpg";
+import classes from "./HeroisHome.module.css"
 
 const HeroisHome = () => {
     return (
-        <div id="herois">
-            <h1>Nem Todos os Heróis Vestem Capas</h1>
-            <Link to="/herois/heroi1">
-                <img src="" alt="Heroi-do-mes-1"/>
-                <p>Nome Héroi 1</p>
+        <div id="herois" className={classes.mainHeroisHome}>
+            <h1 className={classes.mainHeroisTitle}>Nem Todos os Heróis Vestem Capas</h1>
+            <Link to="/herois/heroi1" className={classes.linkHerois1}>
+                <img src={boxesVolunteer} alt="Heroi-do-mes-1" className={classes.heroisImg}/>
+                <p className={classes.heroisName}>Nome Héroi 1</p>
             </Link>
-            <Link to="/herois/heroi2">
-                <img src="" alt="Heroi-do-mes-2"/>
-                <p>Nome Héroi 2</p>
+            <Link to="/herois/heroi2" className={classes.linkHerois2}>
+                <img src={oceanSaving} alt="Heroi-do-mes-2" className={classes.heroisImg}/>
+                <p className={classes.heroisName}>Nome Héroi 2</p>
             </Link>
-            <Link to="/herois/heroi3">
-                <img src="" alt="Heroi-do-mes-3"/>
-                <p>Nome Héroi 3</p>
+            <Link to="/herois/heroi3" className={classes.linkHerois3}>
+                <img src={foodVolunteer} alt="Heroi-do-mes-3" className={classes.heroisImg}/>
+                <p className={classes.heroisName}>Nome Héroi 3</p>
             </Link>
         </div>
     )
 }
 
 export default HeroisHome
-//todo links dinamicos com id... fetch da data dos herois..
+
+//TODO: links dinamicos com id + fetch data dos herois (3 ultimos)
+//Talvez slideshow aqui
