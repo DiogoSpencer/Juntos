15f37a04b41b2ac6e93d8eac6f29085be4b1f608
Niennaz <nicole_oix@hotmail.com>
2021-07-13 14:22:11 +0200

    fixes

--- src/components/ListHelps/ListHelps.js
+++ src/components/ListHelps/ListHelps.js
@@ -1,6 +1,6 @@
 
 import { Fragment, useState } from "react";
-import SideButtons from "../UI/SIdeButtons";
+import SideButtons from "../UI/SideButtons";
 import HelpListItem from "./HelpListItem";
 import HelpAnonimousItem from "./HelpAnonimousItem";
 
