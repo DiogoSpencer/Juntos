9863361d7cd7600ea85e1035a6be983b4fe12c3e
Niennaz <nicole_oix@hotmail.com>
2021-07-25 21:55:49 +0200

    home page app images

--- src/components/Home/AppHome.js
+++ src/components/Home/AppHome.js
@@ -1,15 +1,15 @@
-import { Link } from "react-router-dom";
 import IconButton from "../UI/IconButton";
-import entrar from "../../img/entrar.jpg";
-import inicial from "../../img/inicial.jpg";
-import registar from "../../img/registar.jpg";
+import entrar from "../../img/mapaApp.jpg";
+import inicial from "../../img/chooseApp.jpg";
+import registar from "../../img/detailsApp.jpg";
 import classes from "./AppHome.module.css";
 
 const AppHome = () => {
   return (
     <div id="app" className={classes.mainApp}>
       <h3 className={classes.imgText1}>
-        <span className={classes.numbers}>1.</span> Começa por criar uma conta
+        <span className={classes.numbers}>1.</span> Podes criar diversos tipos
+        de eventos
       </h3>
       <img
         className={`${classes.appImage} ${classes.divImage1}`}
@@ -17,7 +17,9 @@ const AppHome = () => {
         alt="App Preview Register"
       />
       <h3 className={classes.imgText2}>
-        <span className={classes.numbers}>2.</span> Vê quem precisa de ajuda ou <br/>oferece ajuda perto de ti
+        <span className={classes.numbers}>2.</span> Vê quem precisa de ajuda ou{" "}
+        <br />
+        oferece ajuda perto de ti
       </h3>
       <img
         className={`${classes.appImage} ${classes.divImage2}`}
@@ -25,7 +27,8 @@ const AppHome = () => {
         alt="App Preview Register"
       />
       <h3 className={classes.imgText3}>
-        <span className={classes.numbers}>3.</span> Começa por criar uma conta
+        <span className={classes.numbers}>3.</span> Podes ver detalhes dos
+        eventos e ajudar!
       </h3>
       <img
         className={`${classes.appImage} ${classes.divImage3}`}
@@ -35,7 +38,8 @@ const AppHome = () => {
       <div className={classes.appText}>
         <p>Ajudar alguém? Precisas de ajuda? Doar roupas de criança?</p>
         <div className={classes.appLink}>
-          Descarrega a nossa app e vê o que podemos fazer <span className={classes.juntos}>juntos</span>
+          Descarrega a nossa app e vê o que podemos fazer{" "}
+          <span className={classes.juntos}>juntos</span>
         </div>
         <div className={classes.appButton}>
           <IconButton text="Descarrega Aqui!" />
